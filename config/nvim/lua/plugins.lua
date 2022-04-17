2f80a13a4bd3a8efd0b7c01ad01cbdd62cbf8597
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-04-17 00:12:55 -0700

    changed nvim theme to storm

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -12,7 +12,7 @@ require('packer').startup(function(use)
   }
 end)
 
-vim.g.tokyonight_style = "night"
+vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.cmd[[colorscheme tokyonight]]
