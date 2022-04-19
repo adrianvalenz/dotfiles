4ec26f1b9d10eacee95d936e9503a6c879477164
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-04-18 22:19:49 -0700

    added a postico in brew file as comment

--- scripts/macos/brew.sh
+++ scripts/macos/brew.sh
@@ -4,6 +4,7 @@ brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask obs
 brew install --cask slack
+# brew install --cask postico
 
 # libpg is needed to use the native pg gem without Rosetta on M1 macs
 brew install libpq
